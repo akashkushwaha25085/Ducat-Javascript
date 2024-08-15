e425ae6407f5a0aca3ae349363c180a439285c07
akashkushwaha25085 <142476870+akashkushwaha25085@users.noreply.github.com>
2024-08-15 16:11:42 +0530

    Add files via upload

---
+++ 5Days1Assignment.js
@@ -0,0 +1,66 @@
+// Write a JavaScript program that compares two variables and prints the larger one.
+
+// var  num1 = 97;
+// var num2 = 97;
+
+// if(num1 > num2){
+//    console.log('the larger number is : ',num1);
+// }
+// else if(num2 > num1){
+//    console.log('the larger number is : ',num2);
+// }
+// else{
+//    console.log('both  are   equal');
+// }
+
+
+// Write a JavaScript program that assigns a value to a variable representing age.
+// If the age is less than 18, print "You are minor"; otherwise, print "You are an
+// adult".
+
+// var age = 19
+// if(age<=18){
+//    console.log('you are minor');
+// }
+// else{
+//    console.log('you are adult');
+// }
+
+
+// Develop a program in JavaScript that assigns a value to a variable and checks
+// whether it is divisible by 5, printing "Divisible by 5" or "Not divisible by 5"
+// accordingly.
+
+// var number =  1000
+
+// if(number % 5 ===  0){
+//    console.log('divisible by 5');
+// }
+// else{
+//    console.log('Not divisible by 5');
+// }
+
+
+// Develop a program in JavaScript that assigns a value to a variable representing
+// a number and checks whether it is even or odd.
+
+// var number = 99
+// if (number % 2 === 0) {
+//    console.log('this number is even');
+// }
+//  else {
+// console.log('this number is odd');
+// }
+
+
+// Write a JavaScript program that assigns a value to a variable representing a
+// number and checks whether it is positive. If it is positive, print "Positive";
+// otherwise, print "Not positive".
+ 
+var number = -14515
+if(number > 0 ){
+   console.log('positive number');
+}
+else{
+   console.log('not positive');
+}
